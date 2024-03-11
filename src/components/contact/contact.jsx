2c40e2ee81706130c25f94fe4c7f1ab37c17775f
othmane ait salah <othmaneaitsalah@hotmail.com>
2024-03-11 13:45:09 +0000

    main

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -3,9 +3,34 @@ import './contact.css'
 
 const contact = () => {
     return(
-        <div>
-            CONTACT
-        </div>
+        <section className='contact-us'>
+            <h1 className='title'> 
+                <span className='icon-envelope'></span>
+                Contact us
+            </h1>
+            <p className='sub-title'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolorem possimus repudiandae beatae odit. Ad aliquid illum, doloremque id earum molestiae veniam rerum optio</p>
+
+
+            <div className="flex">
+                <form>
+                    <div className='flex' >
+                        <label htmlFor="email">Email Address</label>
+                        <input required type="email" id='email' placeholder='Your email'/>
+                    </div>
+
+                    <div className='flex' style={{marginTop: "24px"}}>
+                        <label htmlFor="msj">Your Message </label>
+                        <textarea required name="msj" id="msj" placeholder='Message'></textarea>
+                    </div>
+
+                    <button className='submit'>Submit</button>
+
+                    <div className="animation">
+                        
+                    </div>
+                </form>
+            </div>
+        </section>
     )
 }
 
